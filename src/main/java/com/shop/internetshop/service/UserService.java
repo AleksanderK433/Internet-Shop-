5df31a26a29e7087ac06237ea1ec1a85a76165e0
AleksanderK433 <ak125135@stud.ur.edu.pl>
2025-09-23 12:22:50 +0200

    logika rejestracji i weryfikacji email

---
+++ src/main/java/com/shop/internetshop/service/UserService.java
@@ -0,0 +1,22 @@
+package com.shop.internetshop.service;
+
+import com.shop.internetshop.model.User;
+import com.shop.internetshop.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
